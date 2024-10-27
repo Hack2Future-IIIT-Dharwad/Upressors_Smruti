237a340a290e7852092355976ba2b0a164064e87
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2024-10-27 09:03:54 +0530

    syntax error

--- client/src/pages/playground/custom-nodes/output-node.tsx
+++ client/src/pages/playground/custom-nodes/output-node.tsx
@@ -112,7 +112,7 @@ export const OutputNode = ({ data }: { data: OutputNodeData }) => {
                             <ChessboardSkeleton />
                         )}
                         <NodeToolbar isVisible={data.toolbarVisible} position={data.toolbarPosition}>
-                            <button onClick={handleFileDownload}>dowload</button>
+                            <button onClick={handleFileDownload}>Download</button>
 
                         </NodeToolbar>
                     </div>
